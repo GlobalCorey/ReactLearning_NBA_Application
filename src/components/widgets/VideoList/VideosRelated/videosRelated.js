768cbb6acebca617c8b26415661a0cc095990281
Corey M. Loftis <cmloftis@techsoft.com>
2018-05-18 16:18:58 -0500

    Working on related videos functionality of project

---
+++ src/components/widgets/VideoList/VideosRelated/videosRelated.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from '../videoList.css'
+
+
+const VideosRelated = (props) => (
+    <div>
+        Videos related component
+    </div>
+)
+
+export default VideosRelated;
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
+import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index';
 
+//Does this NEED to be a class based component?
+    //no state
+    //no lifecycle methods
+    //no refs
 class Routes extends Component {
     render(){
         return(
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
+                    <Route path="/articles:id" exact component={NewsArticle}/>
+                    <Route path="/videos/:id" exact component={VideoArticle}/>
                 </Switch>
             </Layout>
         )
